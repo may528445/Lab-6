d87644a2167572b63963a6bee0196848a95ddb81
may528445 <68011381@kmitl.ac.th>
2025-09-05 05:17:14 +0000

    Update

--- lab-6/ex03.c
+++ lab-6/ex03.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
-static void print_clean_float(double x){
-    char buf[64];
-    snprintf(buf, sizeof(buf), "%.6g", x);
-    fputs(buf, stdout);
-}
+struct Point {
+    float x;
+    float y;
+};
 
-int main(void){
-    double x1,y1,x2,y2;
-    printf("x1: "); scanf("%lf",&x1);
-    printf("y1: "); scanf("%lf",&y1);
-    printf("x2: "); scanf("%lf",&x2);
-    printf("y2: "); scanf("%lf",&y2);
-    double d = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-    printf("Distance between (");
-    print_clean_float(x1); printf(", "); print_clean_float(y1);
-    printf(") and ("); print_clean_float(x2); printf(", "); print_clean_float(y2);
-    printf(") is %.3f unit(s)\n", d);
+int main() {
+    struct Point point1, point2;
+    float distance;
+    
+    printf("x1: ");
+    scanf("%f", &point1.x);
+    
+    printf("y1: ");
+    scanf("%f", &point1.y);
+    
+    printf("x2: ");
+    scanf("%f", &point2.x);
+    
+    printf("y2: ");
+    scanf("%f", &point2.y);
+    
+    distance = sqrt(pow(point2.x - point1.x, 2) + pow(point2.y - point1.y, 2));
+    
+    printf("Distance between (%.1f, %.1f) and (%.1f, %.1f) is %.3f unit(s)\n", 
+           point1.x, point1.y, point2.x, point2.y, distance);
+    
     return 0;
 }
